0a702e3ff43be4003d70452dff37eef6677b0401
RodrigoMurulo <rodrigo.murulo@hotmail.com>
2021-08-19 09:43:30 -0300

    bkp dia 19

---
+++ src/main/java/br/com/isidrocorp/eventdash/model/Alarme.java
@@ -0,0 +1,49 @@
+package br.com.isidrocorp.eventdash.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//mapeamento da tabela
+@Entity // indica que a clsse alarme é armazenavel no banco
+@Table(name = "itmn=alarme") // especifica o nome da tabela
+public class Alarme {
+
+	@Column(name = "id_alarme") // indica o nome da coluna
+	@Id // define que o atributo corresponde a uma PK na tabela
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // pra dizer que é auto-incremente
+	private int id;
+
+	@Column(name = "nome", length = 100, nullable = false)
+	private String nome;
+
+	@Column(name = "descricao", length = 200, nullable = true)
+	private String descricao;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
